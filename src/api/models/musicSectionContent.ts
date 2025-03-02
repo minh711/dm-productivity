a9741344211ed7c5ef5bbeba7625e4c67c8644da
Trương Duy Minh <minhtd.developer@gmail.com>
2025-03-02 20:55:38 +0700

    fix: wrong datatype

--- src/api/models/musicSectionContent.ts
+++ src/api/models/musicSectionContent.ts
@@ -3,9 +3,9 @@ import { MusicTag } from './musicTag';
 export interface MusicSectionContent {
   id: string;
   description: string;
-  pdf?: string[];
-  audio?: string[];
-  notes?: string[];
+  pdf?: string;
+  audio?: string;
+  notes?: string;
   tagIds: string[];
   tags?: MusicTag[];
 }
